b089a3cdd989605e7d8d906925b253be8c589b6f
ivamgf <programador.ivam@gmail.com>
2022-02-10 17:20:00 -0300

    add dockerfile

--- api/src/api/class/routes/class.js
+++ api/src/api/class/routes/class.js
@@ -6,19 +6,4 @@
 
 const { createCoreRouter } = require('@strapi/strapi').factories;
 
-module.exports = createCoreRouter('api::class.class', {
-  prefix: '',
-  only: ['find', 'findOne'],
-  except: [],
-  config: {
-    find: {
-      auth: false,
-      policies: [],
-      middlewares: [],
-    },
-    findOne: {},
-    create: {},
-    update: {},
-    delete: {},
-  },
-});
+module.exports = createCoreRouter('api::class.class');
